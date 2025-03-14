e70f50442877e0379e6e050f17c7e8e78d48e09b
Matthew Dang <dangmq1@uci.edu>
2025-03-14 04:02:20 +0000

    Fixed printing

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -48,7 +48,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 void print_path(const vector<int>& path, int total) {
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
-        if (i < path.size() - 1) cout << " ";
+        cout << " ";
     }
     cout << "\nTotal cost is " << total << endl;
 }
