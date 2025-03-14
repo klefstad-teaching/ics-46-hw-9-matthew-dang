868792582ce77f8aab6ca113ade40719faf93211
Matthew Dang <dangmq1@uci.edu>
2025-03-14 02:33:32 +0000

    completed edit distance within

---
+++ src/ladder.cpp
@@ -0,0 +1,28 @@
+#include "ladder.h"
+
+void error(string word1, string word2, string msg) {
+    cerr << "Error: " << msg << " (" << word1 << ", " << word2 << ")" << endl;
+}
+
+bool edit_distance_within(const string& str1, const string& str2, ind d) {
+    int len1 = str1.size(), len2 = str2.size();
+    if (abs(len1 - len2) > 1) return false;
+
+    int diff_count = 0, i = 0, j = 0
+    while (i < len1 && j < len2) {
+        if (str1[i] != str2[j]) {
+            if (++diff_count > d) return false;
+            if (len1 > len2) i++;
+            else if (len1 < len2) j++;
+            else { i++; j++; }
+        } else {
+            i++; j++;
+        }
+    }
+    return true;
+}
+
+bool is_adjacent(const string& word1, const string& word2) {
+    return edit_distance_within(word1, word2, 1);
+}
+
